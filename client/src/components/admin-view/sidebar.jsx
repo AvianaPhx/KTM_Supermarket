5b6b51e7febc627ad65c66d959f3570f70b1d369
Arabinda Krishna Kansakar <61239985+AvianaPhx@users.noreply.github.com>
2024-12-23 13:02:32 +0545

    Admin dash Layout

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,9 +1,15 @@
 import {
-    BadgeCheck,
-    ChartNoAxesCombined,
-    LayoutDashboard,
-    ShoppingBasket,
-  } from "lucide-react";
+  BadgeCheck,
+  ChartNoAxesCombined,
+  LayoutDashboard,
+  ShoppingBasket,
+} from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet"
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -28,43 +34,59 @@ const adminSidebarMenuItems = [
     },
   ];
 
-function MenuItems() {
-    const navigate = useNavigate();
+function MenuItems({setOpen}) {
+  const navigate = useNavigate();
 
-    return (
-        <nav className="mt-8 flex-col flex gap-2">
-          {adminSidebarMenuItems.map((menuItem) => (
-            <div
-              key={menuItem.id}
-              onClick={() => {
-                navigate(menuItem.path);
-              }}className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground">
-              {menuItem.icon}
-              <span>{menuItem.label}</span>
-            </div>
-          ))}
-        </nav>
-      );
+  return (
+    <nav className="mt-8 flex-col flex gap-2">
+      {adminSidebarMenuItems.map((menuItem) => (
+        <div
+          key={menuItem.id}
+          onClick={() => {
+            navigate(menuItem.path)
+            setOpen ? setOpen(false) : null;
+          }}className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground">
+          {menuItem.icon}
+          <span>{menuItem.label}</span>
+        </div>
+      ))}
+    </nav>
+  );
 }
 
-function AdminSideBar( ) {
+function AdminSideBar({open, setOpen}) {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    return (
-        <Fragment>
-          <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
-            <div
-              onClick={() => navigate("/admin/dashboard")}
-              className="flex cursor-pointer items-center gap-2"
-            >
+  return (
+    <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col h-full"> 
+          <SheetHeader className="border-b">
+            <SheetTitle className='flex gap-2 mt-5 mb-5'>
               <ChartNoAxesCombined size={30} />
-              <h1 className="text-2xl font-extrabold">Admin Panel</h1>
-            </div>
-            <MenuItems />
-          </aside>
-        </Fragment>
-      );
+              <span className="text-2xl font-extrabold">Admin Panel</span>
+            </SheetTitle>
+          </SheetHeader>
+          <MenuItems setOpen={setOpen} />
+          </div>
+
+        </SheetContent>
+
+      </Sheet>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+        <div
+          onClick={() => navigate("/admin/dashboard")}
+          className="flex cursor-pointer items-center gap-2"
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+        </div>
+        <MenuItems />
+      </aside>
+    </Fragment>
+    );
 }
 
 export default AdminSideBar;
